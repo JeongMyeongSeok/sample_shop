131698ff41047539ee85eee6dcd5052a184223e8
鄭 明錫 <myeongseok.jeong@litalico.co.jp>
2015-07-13 18:02:54 +0900

    住所表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,12 @@
 class OrdersController < ApplicationController
+
   def address
     @address = current_user.commonly_used_destinations.build
     @address_list = CommonlyUsedDestination.where("user_id = ?", current_user.id)
+  end
+
+  def confirm
+    @address = CommonlyUsedDestination.find_by("id = ?", params[:id])
+    @products_in_cart = CartDetail.where("user_id = ?", current_user.id)
   end
 end
