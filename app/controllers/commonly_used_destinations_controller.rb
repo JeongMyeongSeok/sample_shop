fbb8e8c188565f2d3cf377b2fac8e6c46e6febfc
鄭 明錫 <myeongseok.jeong@litalico.co.jp>
2015-07-13 16:34:35 +0900

    complete_address_edit

--- app/controllers/commonly_used_destinations_controller.rb
+++ app/controllers/commonly_used_destinations_controller.rb
@@ -1,4 +1,6 @@
 class CommonlyUsedDestinationsController < ApplicationController
+  before_action :selected_address_for_edit, only: [:edit, :update]
+
   def create
     @address = CommonlyUsedDestination.new(address_params)
     if @address.save
@@ -10,7 +12,35 @@ class CommonlyUsedDestinationsController < ApplicationController
     end
   end
 
+  def destroy
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @address.update_attributes(edit_address_params)
+      flash[:success] = "住所変更"
+      redirect_to address_order_path(current_user)
+    else
+      render "edit"
+    end
+  end
+
   private
+
+  def selected_address_for_edit
+    @address = CommonlyUsedDestination.find_by(id: params[:id], user_id: current_user.id)
+    if @address.nil?
+      redirect_to root_url
+    end
+  end
+
+  def edit_address_params
+    params.require(:commonly_used_destination).permit(:name, :address, :phone)
+  end
 
   def address_params
     params.require(:commonly_used_destination).permit(:user_id, :name, :address, :phone)
